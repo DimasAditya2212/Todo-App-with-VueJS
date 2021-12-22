25324c376267eb1327e5739a94f48bf104fdb66d
DimasAditya2212 <dimas.aditya2295@gmail.com>
2021-12-22 17:54:16 +0700

    ver1: making first ui

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text='Add Task' color='grey'/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    components: {
+        Button
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'hello',
+        },
+    },
+    
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>>
